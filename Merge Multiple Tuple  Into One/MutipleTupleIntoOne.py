620c8ff4fc3866dfabb5c27001b0dc1bd3cfbbe2
sabitharajendran1311 <sabitharajendran2003@gmail.com>
2024-07-03 11:11:52 +0530

    Merge muiple tuple into one

---
+++ Merge Multiple Tuple  Into One/MutipleTupleIntoOne.py
@@ -0,0 +1,8 @@
+tuple1=(1,"apple",3.14)
+tuple2=("banana",5,6.78)
+tuple3=(7.89,8,"cherry")
+merged_tuple=tuple1+tuple2+tuple3
+print("Tuple1:",tuple1)
+print("Tuple2:",tuple2)
+print("Tuple3:",tuple3)
+print("Merged Tuple:",merged_tuple)

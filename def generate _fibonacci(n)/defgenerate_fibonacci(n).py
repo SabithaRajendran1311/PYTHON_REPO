4be4d9ab114a7029a805ca058c6c4e5200d3de98
sabitharajendran1311 <sabitharajendran2003@gmail.com>
2024-07-04 10:22:32 +0530

    def generate_fibonacci(n)

---
+++ def generate _fibonacci(n)/defgenerate_fibonacci(n).py
@@ -0,0 +1,10 @@
+def generate_fibonacci(n):
+    sequence=[0,1]
+    while len(sequence)<n:
+        next_num=sequence[-1]+sequence[-2]
+        sequence.append(next_num)
+    return sequence
+num_terms=int(input("Enter the number of terms:"))
+fibonacci_seq=generate_fibonacci(num_terms)
+print("fibonacci sequence:",fibonacci_seq)
+    

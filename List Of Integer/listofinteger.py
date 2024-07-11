909927bb0eb9e8e8ae2226b6d3cb28c51cdb8cd4
sabitharajendran1311 <sabitharajendran2003@gmail.com>
2024-07-11 10:20:28 +0530

    list of integer

---
+++ List Of Integer/listofinteger.py
@@ -0,0 +1,7 @@
+num_list=[]
+n=int(input("Enter the number of elements:"))
+for i in range(n):
+    num=int(input("enter an integer :"))
+    num_list.append(num)
+    print("List of integers:",num_list)
+

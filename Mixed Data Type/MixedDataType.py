07645160d5e91db353a94ee1ce7286fc524d180d
sabitharajendran1311 <sabitharajendran2003@gmail.com>
2024-07-11 10:29:51 +0530

    Mixed Data Type

---
+++ Mixed Data Type/MixedDataType.py
@@ -0,0 +1,6 @@
+mixed_list=[]
+n=int(input("Enter the number of element:"))
+for i in range(n):
+    element=input("Enetr an element:")
+    mixed_list.append(element)
+    print("List of mixed data types:",mixed_list)

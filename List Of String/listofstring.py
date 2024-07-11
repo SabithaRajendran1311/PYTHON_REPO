d2b3d2074e0c4edb84a39233b2ccfa268babf2df
sabitharajendran1311 <sabitharajendran2003@gmail.com>
2024-07-11 10:25:25 +0530

    List Of String

---
+++ List Of String/listofstring.py
@@ -0,0 +1,8 @@
+
+str_list=[]
+n=int(input("Enter the number of elements:"))
+for i in range(n):
+    string=input("enter a string:")
+    str_list.append(string)
+print("List of strings:",str_list) 
+
